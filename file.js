d612ce15d0d2466b18c9ebae0f77d7208ccfeebc
Harrison <harrison.tecplaner@outlook.com>
2018-05-27 23:08:48 -0300

    eeeeee

--- file.js
+++ file.js
@@ -1,17 +1,3 @@
 //- Javascript aqui.....
 
 var fc_abreArquivo = file_s => alert('Abrindo arquivo ' + file_s);
-
-var fc_copiaArquivo = file_s => {
-    console.log("copiando arquivo");
-}
-var fc_excluiArquivo = (file) => {
-    console.log("Exluir arquivo");
-}
-
-//
-// Formatar aquivo
-//
-var fc_formataArquivo = () => {
-    console.log("Formatando Arquivo");
-}
