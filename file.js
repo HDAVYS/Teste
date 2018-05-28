aa9cc5ffbfa66ea5223b29be7ab932f313675a37
Harrison <harrison.tecplaner@outlook.com>
2018-05-27 21:33:20 -0300

    Implementando Mover Arquivo

--- file.js
+++ file.js
@@ -5,6 +5,12 @@ var fc_abreArquivo = file_s => alert('Abrindo arquivo ' + file_s);
 var fc_copiaArquivo = (source, dest) => {
     console.log("Copiando arquivo");
 }
+
 var fc_deleteArquivo = file_s => {
     console.log("Excluindo Arquivo");
+}
+
+//Move Arquivo
+var fc_moveArquivo = (source, dest) => {
+    console.log("Movendo Arquivo!!!");
 }
