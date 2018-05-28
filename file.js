16b76fdfb91c1aff3ebbb8016cdf8593191c67ac
Harrison Davys <harrison.tecno@gmail.com>
2018-05-27 22:37:35 -0300

    Limpando arquivo

--- file.js
+++ file.js
@@ -1,28 +1,3 @@
 //- Javascript aqui.....
 
 var fc_abreArquivo = file_s => alert('Abrindo arquivo ' + file_s);
-
-var fc_copiaArquivo = (source, dest) => {
-    console.log("Copiando arquivo");
-}
-
-var fc_deleteArquivo = file_s => {
-    console.log("Excluindo Arquivo");
-}
-
-//- Realiza a Verificação de Arquivos
-var fc_verificaArquivo = file_s => {
-    console.log("Verificando Arquivo");
-}
-//Move Arquivo
-var fc_moveArquivo = (source, dest) => {
-    console.log("Movendo Arquivo!!!");
-}
-
-var fc_formataArquivo = (file_s) => {
-    console.log("Formatando Arquivo");
-}
-
-var fc_comparaArquivo = (file1, file2) => {
-    console.log("Comparando Arquivos");
-}
